273c2143cbc99be9b046b25ad05c68afa09d4026
akbar19910918 <akbarachilov91@gmail.com>
2023-03-17 23:12:05 +0500

    string

---
+++ 9-dars/index.js
@@ -0,0 +1,79 @@
+// let str="webbrain"
+// console.log(str[0]);//  [0]degani 0chi index 1-elementni ol degagi. w 1-element
+
+//let str="webbrain"
+// console.log(str.charAt(2)); // bu xam xuddi shundek b 2-index,3-element.
+
+// let str="webbrain"
+// console.log(str.at(-1));//
+
+// let str="webbrain"
+// console.log(str.toUpperCase(2));// bu xamma xarfni katta qilib beradi.
+
+// let str="WEBBRAIN"
+// console.log(str.toLowerCase());// xamma xarfni kichik qilib beradi
+
+// let str="webbrain"
+// console.log(str.indexOf("brain"));// 3 "brain" nechanchi indexdan boshlanganini korsatadi
+
+// let str="Webbrain It web"
+// console.log(str.indexOf("web",5));//bu degani webni 5-indexdan keyin qidirib nechanchi indexda ekanini korsatadi
+
+//let str="Webbrain It web"
+//console.log(str.lastIndexOf("web")); // oxiridan boshlab malumot qidiradi
+
+// let str="Webbrain It web"
+// console.log(str.includes("web"));// true folse qiymat qaytaradi.web yozuvi bor yoki yoqligini tekshiradi
+
+// let str="+998972037552"
+// console.log(str.startsWith("+998"));//true folse qiymat qaytaradi.stringni boshidan shu ni qidiradi
+
+// let str="+998972037552"
+// console.log(str.endsWith("7552"));// bu oxiridan qidiradi
+
+// let str="972037552"
+// console.log(str.padStart( 13,"+998"));// "972037552"ni oldiga "+998"ni qoshib qiyish.
+// 13 esa jami stringlar soni.agar 12 deb bersak 8ni tahsab ketadi.11 deb bersak 9ni.
+
+// let str="+998972037552"
+// console.log(str.slice(4));// 4-indexdan boshlab kesb oladi.972037552 chiqadi
+
+// let str="+998972037552"
+// console.log(str.slice(9,13));//9-indexdan 13-indexgacha kesb oladi
+
+// let str="+998972037552"
+// console.log(str.slice(-4,-2)); // oxirida sanab boshlaydi
+// let str="+998972037552"
+// console.log(str.substring(5,8)); //bu xam xuddi slice bn bir xil.faqat slice - lik qiymat oladi.
+// substring - lik qiymat olmidi
+
+// let str="+998972037552"
+// console.log(str.substr(3,7));//3-indexdan boshlab 7ta index kesb oladi.8972037
+
+// let str="1234567"
+// console.log(str.eval("3+4+1"));
+
+// let str="  1234567  "
+// console.log(str.trim());// stringni boshi va oxiridagi bosh joylani ochiradi
+
+// let str="  1234567  "
+// console.log(str.trimStart());// faqat boshidagi bosh joyni ochiradi
+
+// let str="  1234567  "
+// console.log(str.trimEnd());// faqat oxiridan bosh joy ochiradi
+
+// let str="1234567"
+// console.log(str.split());// ['1234567'] malumotlarni arrayga otkazb beradi.
+
+// let str="1234567"
+// console.log(str.concat("890"));//1234567890 chiqadi.890ni qoshib qoydi
+
+// let str="salom"
+// for(i of str){
+ //   console.log(i);
+// } // string ichidagi malumotni chiqaradi.
+
+// let str="salom"
+// for(i in str){
+//    console.log(i);
+//  }     // index raqamini chiqaradi
